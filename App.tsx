29ef48f671d9ba1ea3663330bbefcd0093174cf9
Alexander Mendivil <alexanderima@outlook.es>
2022-01-29 19:57:47 -0700

    Se agrego axios y llamada a api

--- App.tsx
+++ App.tsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Text, View } from 'react-native';
+import axios from 'axios';
+
+const getPopularMovies = async () =>{
+  const response = await axios.get("https://api.themoviedb.org/3/movie/popular?api_key=06346bd5142a8959b8b0e8df0606be39")
+  return response.data.results
+}
 
 const App = () => {
+  const [movies, setMovies] = useState<Object[]>([])
+ 
+  useEffect(()=>{
+    getPopularMovies().then((movie:Object[])=>{
+      setMovies(movie)
+    })
+  },[])
+
   return (
     <View
       style={{
@@ -9,7 +23,8 @@ const App = () => {
         justifyContent: "center",
         alignItems: "center"
       }}>
-      <Text>Hello, world!</Text>
+      <Text>Movie name: {movies[0].original_title || "titulo"}</Text>
+      {/* <Text>Movie language: {JSON.stringify(movies[0].original_language) || "titulo"}</Text> */}
     </View>
   )
 }
