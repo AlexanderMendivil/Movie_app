ebcb494e8dc96385797869c67cdffe22aabbd3bd
Alexander Mendivil <alexanderima@outlook.es>
2022-01-30 19:13:07 -0700

    Se agrego type del moviemodel

--- App.tsx
+++ App.tsx
@@ -1,21 +1,26 @@
 import React, { useEffect, useState } from 'react';
 import { Text, View } from 'react-native';
-import axios from 'axios';
 
+import axios from 'axios';
+import { MovieModel } from './Models/MovieModel';
 const getPopularMovies = async () =>{
   const response = await axios.get("https://api.themoviedb.org/3/movie/popular?api_key=06346bd5142a8959b8b0e8df0606be39")
   return response.data.results
 }
 
 const App = () => {
-  const [movies, setMovies] = useState<Object[]>([])
- 
-  useEffect(()=>{
-    getPopularMovies().then((movie:Object[])=>{
-      setMovies(movie)
-    })
-  },[])
 
+  const [movies, setMovies] = useState<MovieModel[]>([])
+  const [error, setError] = useState<string>("");
+  useEffect(() => {
+
+    const result = getPopularMovies()
+    result.then((movie:MovieModel[])=>{
+      setMovies(JSON.parse(JSON.stringify(movie)))
+    }).catch(()=>setError("There was a problem with the movies, sorry!"))
+
+  },[])
+ 
   return (
     <View
       style={{
@@ -23,8 +28,11 @@ const App = () => {
         justifyContent: "center",
         alignItems: "center"
       }}>
-      <Text>Movie name: {movies[0].original_title || "titulo"}</Text>
-      {/* <Text>Movie language: {JSON.stringify(movies[0].original_language) || "titulo"}</Text> */}
+        {error === ""?
+      <Text>Movie name: {movies[10].original_title || "titulo"}</Text>
+      :
+      <Text>Error: {error}</Text>
+        }
     </View>
   )
 }
