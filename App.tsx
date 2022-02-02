ecb5b4000ec7944c6bc6ca78d7aad6060c6d13e1
Alexander Mendivil <alexanderima@outlook.es>
2022-02-01 20:54:06 -0700

    Se limpio app

--- App.tsx
+++ App.tsx
@@ -1,23 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import { Text, View } from 'react-native';
-
-import axios from 'axios';
-import { MovieModel } from './Models/MovieModel';
-import { getPopularMovies } from './services/services';
+import React from 'react';
+import { View } from 'react-native';
+import { Home } from './Screens/Home';
 
 const App = () => {
 
-  const [movies, setMovies] = useState<MovieModel[]>([])
-  const [error, setError] = useState<string>("");
-  useEffect(() => {
-
-    const result = getPopularMovies()
-    result.then((movie:MovieModel)=>{
-      setMovies(JSON.parse(JSON.stringify(movie)))
-    }).catch(()=>setError("There was a problem with the movies, sorry!"))
-
-  },[])
- 
   return (
     <View
       style={{
@@ -25,12 +11,8 @@ const App = () => {
         justifyContent: "center",
         alignItems: "center"
       }}>
-        {error === ""?
-      <Text>Movie name: {JSON.stringify(movies[11].original_title) || "titulo"}</Text>
-      :
-      <Text>Error: {error}</Text>
-        }
-    </View>
+        <Home/>
+      </View>
   )
 }
 export default App;
