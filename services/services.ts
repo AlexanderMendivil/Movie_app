0c22dbc88416141853ae59c0dae2e1795ee00afe
Alexander Mendivil <alexanderima@outlook.es>
2022-02-03 19:18:22 -0700

    Se corrigió el type de las funciones

--- services/services.ts
+++ services/services.ts
@@ -5,13 +5,13 @@ import { MovieModel } from "../Models/MovieModel"
 const URL_API:string = "https://api.themoviedb.org/3"
 const API_KEY:string = "api_key=06346bd5142a8959b8b0e8df0606be39"
 
-export const getPopularMovies = async ():Promise<MovieModel> => {
+export const getPopularMovies = async ():Promise<MovieModel[]> => {
     const response = await axios.get(`${URL_API}/movie/popular?${API_KEY}`)
     return response.data.results
 
   }
   
-export const getUpcomingMovies = async ():Promise<MovieModel> => {
+export const getUpcomingMovies = async ():Promise<MovieModel[]> => {
     const response = await axios.get(`${URL_API}/movie/upcoming?${API_KEY}`)
     return response.data.results
       
