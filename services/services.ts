f3d67c1c8343cb40b01f01c74d3ebf1724d78c11
Alexander Mendivil <alexanderima@outlook.es>
2022-02-08 18:48:14 -0700

    Se agregó llamada a family movies y se cambió el type de la función getPopulatTv

--- services/services.ts
+++ services/services.ts
@@ -1,6 +1,7 @@
 
 import axios from "axios"
 import { MovieModel } from "../Models/MovieModel"
+import { tvModel } from "../Models/tvModel"
 
 const URL_API:string = "https://api.themoviedb.org/3"
 const API_KEY:string = "api_key=06346bd5142a8959b8b0e8df0606be39"
@@ -17,8 +18,14 @@ export const getUpcomingMovies = async ():Promise<MovieModel[]> => {
       
     }
     
-export const getPopularTv = async () => {
+export const getPopularTv = async ():Promise<tvModel[]> => {
     const response = await axios.get(`${URL_API}/tv/popular?${API_KEY}`)
+    return response.data.results
+    
+    }
+
+export const getFamilyMovies = async ():Promise<MovieModel[]> => {
+    const response = await axios.get(`${URL_API}/discover/movie?${API_KEY}&with_genre=10751`)
     return response.data.results
     
     }
