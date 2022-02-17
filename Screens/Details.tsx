1f08a96385ae148be4d6329e3efec1fc130fc0d9
Alexander Mendivil <alexanderima@outlook.es>
2022-02-16 19:22:48 -0700

    Se leen parametros de navigation y se despliega nombre de la pelicula.

--- Screens/Details.tsx
+++ Screens/Details.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { Text, View } from "react-native";
 
-export const Details = () => {
+export const Details = ({ route }) => {
+
+    const movieDetail  = route.params.movieDetail;
+
     return (
     <React.Fragment>
-        <Text>{"Movie Detail"}</Text>
+        <Text>{movieDetail.title}</Text>
     </React.Fragment>       
     );
 }
