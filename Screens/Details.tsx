d13ac7ae8f04fb49c4cdfd31816554f5bb6caf47
Alexander Mendivil <alexanderima@outlook.es>
2022-02-17 17:37:55 -0700

    Se usa service para detalles de la pelicula y se despliega la imagen.

--- Screens/Details.tsx
+++ Screens/Details.tsx
@@ -1,13 +1,45 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Image, ScrollView, StyleSheet, Text, View, Dimensions, ActivityIndicator } from "react-native";
+import { MovieDetailModel } from "../Models/MovieDetailModel";
+import { getMovie } from "../services/services";
+
+
+const placeHolderImage = require("../Assets/images/placeholder.png");
+const height = Dimensions.get("screen").height;
 
 export const Details = ({ route }) => {
+    const movieId:number  = route.params.movieId;
 
-    const movieDetail  = route.params.movieDetail;
+    const [movieDetail, setMovieDetail] = useState<MovieDetailModel>({});
+    const [ loaded, setLoaded ] = useState<boolean>(false);
+
+    useEffect(()=>{
+        getMovie( movieId ).then(( movieData: MovieDetailModel ) =>{
+            setMovieDetail( movieData );
+            setLoaded(true);
+        })
+    },[movieId])
 
     return (
     <React.Fragment>
-        <Text>{movieDetail.title}</Text>
+        {
+        loaded && (
+        <ScrollView>
+                <Image resizeMode="cover" source={
+                movieDetail.poster_path ?
+                {uri: "https://image.tmdb.org/t/p/w500"+movieDetail.poster_path}
+                : 
+                placeHolderImage
+                } style={styles.movieImage} />
+        </ScrollView>
+        )}
+        {!loaded &&(<ActivityIndicator size="large" color="#000000" />)}
     </React.Fragment>       
     );
 }
+
+const styles = StyleSheet.create({
+    movieImage:{
+        height: height/2,
+    },
+})
