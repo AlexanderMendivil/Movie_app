90667128980c5382f8b16bb3bf92ed83a848dae1
Alexander Mendivil <alexanderima@outlook.es>
2022-03-03 12:26:14 -0700

    Se creó mainnavigation

---
+++ Components/MainNavigation.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import { Details } from '../Screens/Details';
+import { Navbar } from './Navbar';
+import { NavbarHomeLeft } from './Navbar';
+import { NavbarHomeRight } from './Navbar';
+import { Home } from '../Screens/Home';
+
+const Stack = createNativeStackNavigator();
+
+export const MainNavigation = () => {
+    return (
+        <Stack.Navigator >
+
+        <Stack.Screen name="Home" options={
+          ({ navigation })=>({
+
+            headerTransparent:true,
+              headerLeft: ()=><NavbarHomeLeft navigation={ navigation }/>,
+              headerRight: () => <NavbarHomeRight navigation={ navigation }/>,
+              title: ""
+
+            })} 
+            component={Home} />
+  
+        <Stack.Screen name="Detail" options={
+          ({ navigation })=>({
+
+              headerTransparent:true,
+              headerRight: () => <Navbar navigation={ navigation } />,
+              title:""
+              
+            })} 
+            component={Details} />
+  
+      </Stack.Navigator>        
+    );
+}
